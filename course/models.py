7063764113e678f1103e53d14db6ddbeea7d9703
David-Ramboy <davidbryanramboy@gmail.com>
2023-02-22 11:22:55 +0800

    Add models OrderedCourse & Status

--- course/models.py
+++ course/models.py
@@ -14,6 +14,25 @@ class Course(models.Model):
     category = models.ForeignKey(Category, related_name='courses',on_delete=models.CASCADE)
     course = models.CharField(max_length=255)
     duration = models.CharField(max_length=255) 
+    price = models.FloatField(default=0)
 
     def __str__(self):
         return self.course
+
+
+class Status(models.Model):
+    status = models.CharField(max_length=2)
+    
+    def __str__(self):
+        return self.status
+
+class Ordered_Course(models.Model):
+    category = models.ForeignKey(Category, related_name='ordered_courses',on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, related_name='ordered_courses',on_delete=models.CASCADE)
+    price = models.ForeignKey(Course, related_name='ordered_courses_price',on_delete=models.CASCADE)
+    proof_of_payment = models.ImageField(upload_to='payment_images', blank=True, null=True)
+    status = models.ForeignKey(Status, related_name="ordered_courses",on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.course
+    
